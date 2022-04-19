2e0ecd45f1ce1b8dbd248b972909e8640b87eefd
Abhinandan Kumar <Abhinandan Kumar@LAPTOP-DP0AQKQ3>
2022-04-19 02:21:58 -0500

    done

--- LearningJava/src/Unit_01/P2_Task01_ConstantsInJava.java
+++ LearningJava/src/Unit_01/P2_Task01_ConstantsInJava.java
@@ -1,5 +1,34 @@
 package Unit_01;
-
+//constant can never be change once a value is assigned.
 public class P2_Task01_ConstantsInJava {
+  public static final double pi = 3.14;
+	//pi = 3.18;
+	public static void main(String[] args) {
+		System.out.println(pi);
+		main("askbdfhksd");
+		
+		NewClass obj = new NewClass();
+		obj.add(1, 2);
+		//P2_Task02_ConstantsInJava obj1 = new P2_Task02_ConstantsInJava();
+		//obj1.add(2, 2);
+		add(4,7);
+		
+	}
+	
+	public static void main(String args) {
+		System.out.println(pi);
+	}
+	
+	static void add(int a,int b) {
+		System.out.println(a+b);
+		System.out.println(pi);
+	}
+}
 
+class NewClass{
+	//final double pi = 3.14;
+	void add(int a,int b) {
+		System.out.println(a+b);
+		System.out.println(P2_Task02_ConstantsInJava.pi);
+      }
 }
